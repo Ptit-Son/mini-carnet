83e95d4b868fe264bd76696058af8bd74053ea42
jean <jeanmybook@gmail.com>
2026-03-18 03:45:07 +0100

    Ajout de la fonction nombre de taches

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 def afficher_taches(taches):
     print("Liste des tâches :")
     for i, tache in enumerate(taches, start=1):
-        print(f"{i}. {tache}")
+        print(f"{i}.{tache}_")
+        
+        
+def nbre_taches(taches):
+	print("nombre taches: ", len(taches))
